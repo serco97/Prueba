4364f0103e65cffab064aea2d876c45b002405b7
serco97 <serco97@outlook.es>
2017-06-04 13:57:41 +0200

    Update HolaMundo.java

--- Hola Mundo/src/HolaMundo.java
+++ Hola Mundo/src/HolaMundo.java
@@ -4,5 +4,6 @@ public class HolaMundo {
 public static void main(String[] args) {
 	System.out.println("Hola Mundo");
 	System.out.println("pruebade commit");
+	System.out.println("prueba de pull");
 	}
 }
